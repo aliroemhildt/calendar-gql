383e59784ad76ee5960174916f2ec0b34331e378
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-09-13 14:25:12 -0600

    Convert from TS to JS

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const resolvers = require('./resolvers.ts');
 const typeDefs = require('./schema.ts'); // why can it not be named typeDefs in the other file?
 const UserAPI = require('./data/user-api');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      userAPI: new UserAPI()
+    }
+  }
+});
+
 server.listen().then(({ url }) => {
   console.log(`🫡 Server ready at ${url}`);
 });
