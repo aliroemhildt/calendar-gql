e4c9b827159f06be990fbb6d7baeb590a7cf1bb6
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-09-14 13:53:44 -0600

    Add getSingleUser resolver

--- resolvers.js
+++ resolvers.js
@@ -2,6 +2,10 @@ const resolvers = {
   Query: {
     users: (_, __, { dataSources }) => {
       return dataSources.userAPI.getUsers();
+    },
+
+    user: ({ id }, _, { dataSources }) => {
+      return dataSources.userAPI.getSingleUser(id);
     }
   }
 }
